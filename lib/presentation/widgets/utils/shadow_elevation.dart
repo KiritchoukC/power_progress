e441378f170a43bdc07520d7e5aa99e547e8c746
KiritchoukC <clement.kiritchouk@gmail.com>
2020-08-08 21:07:34 +0200

    feat(shadow-elevation): add all elevation as shadows

--- lib/presentation/widgets/utils/shadow_elevation.dart
+++ lib/presentation/widgets/utils/shadow_elevation.dart
@@ -64,4 +64,149 @@ List<BoxShadow> elevation3 = const <BoxShadow>[
   ),
 ];
 
-/* offset-x | offset-y | blur-radius | spread-radius | color */
+List<BoxShadow> elevation4 = const <BoxShadow>[
+  BoxShadow(
+    offset: Offset(0, 4),
+    blurRadius: 5,
+    spreadRadius: 0,
+    color: Color.fromRGBO(0, 0, 0, 0.14),
+  ),
+  BoxShadow(
+    offset: Offset(0, 1),
+    blurRadius: 10,
+    spreadRadius: 0,
+    color: Color.fromRGBO(0, 0, 0, 0.12),
+  ),
+  BoxShadow(
+    offset: Offset(0, 2),
+    blurRadius: 4,
+    spreadRadius: -1,
+    color: Color.fromRGBO(0, 0, 0, 0.20),
+  ),
+];
+
+List<BoxShadow> elevation6 = const <BoxShadow>[
+  BoxShadow(
+    offset: Offset(0, 6),
+    blurRadius: 10,
+    spreadRadius: 0,
+    color: Color.fromRGBO(0, 0, 0, 0.14),
+  ),
+  BoxShadow(
+    offset: Offset(0, 1),
+    blurRadius: 18,
+    spreadRadius: 0,
+    color: Color.fromRGBO(0, 0, 0, 0.12),
+  ),
+  BoxShadow(
+    offset: Offset(0, 3),
+    blurRadius: 5,
+    spreadRadius: -1,
+    color: Color.fromRGBO(0, 0, 0, 0.20),
+  ),
+];
+
+List<BoxShadow> elevation8 = const <BoxShadow>[
+  BoxShadow(
+    offset: Offset(0, 8),
+    blurRadius: 10,
+    spreadRadius: 1,
+    color: Color.fromRGBO(0, 0, 0, 0.14),
+  ),
+  BoxShadow(
+    offset: Offset(0, 3),
+    blurRadius: 14,
+    spreadRadius: 2,
+    color: Color.fromRGBO(0, 0, 0, 0.12),
+  ),
+  BoxShadow(
+    offset: Offset(0, 5),
+    blurRadius: 5,
+    spreadRadius: -3,
+    color: Color.fromRGBO(0, 0, 0, 0.20),
+  ),
+];
+
+List<BoxShadow> elevation9 = const <BoxShadow>[
+  BoxShadow(
+    offset: Offset(0, 9),
+    blurRadius: 12,
+    spreadRadius: 1,
+    color: Color.fromRGBO(0, 0, 0, 0.14),
+  ),
+  BoxShadow(
+    offset: Offset(0, 3),
+    blurRadius: 16,
+    spreadRadius: 2,
+    color: Color.fromRGBO(0, 0, 0, 0.12),
+  ),
+  BoxShadow(
+    offset: Offset(0, 5),
+    blurRadius: 6,
+    spreadRadius: -3,
+    color: Color.fromRGBO(0, 0, 0, 0.20),
+  ),
+];
+
+List<BoxShadow> elevation12 = const <BoxShadow>[
+  BoxShadow(
+    offset: Offset(0, 12),
+    blurRadius: 17,
+    spreadRadius: 2,
+    color: Color.fromRGBO(0, 0, 0, 0.14),
+  ),
+  BoxShadow(
+    offset: Offset(0, 5),
+    blurRadius: 22,
+    spreadRadius: 4,
+    color: Color.fromRGBO(0, 0, 0, 0.12),
+  ),
+  BoxShadow(
+    offset: Offset(0, 7),
+    blurRadius: 8,
+    spreadRadius: -4,
+    color: Color.fromRGBO(0, 0, 0, 0.20),
+  ),
+];
+
+List<BoxShadow> elevation16 = const <BoxShadow>[
+  BoxShadow(
+    offset: Offset(0, 16),
+    blurRadius: 24,
+    spreadRadius: 2,
+    color: Color.fromRGBO(0, 0, 0, 0.14),
+  ),
+  BoxShadow(
+    offset: Offset(0, 6),
+    blurRadius: 30,
+    spreadRadius: 5,
+    color: Color.fromRGBO(0, 0, 0, 0.12),
+  ),
+  BoxShadow(
+    offset: Offset(0, 8),
+    blurRadius: 10,
+    spreadRadius: -5,
+    color: Color.fromRGBO(0, 0, 0, 0.20),
+  ),
+];
+
+List<BoxShadow> elevation24 = const <BoxShadow>[
+  BoxShadow(
+    offset: Offset(0, 24),
+    blurRadius: 38,
+    spreadRadius: 3,
+    color: Color.fromRGBO(0, 0, 0, 0.14),
+  ),
+  BoxShadow(
+    offset: Offset(0, 9),
+    blurRadius: 46,
+    spreadRadius: 8,
+    color: Color.fromRGBO(0, 0, 0, 0.12),
+  ),
+  BoxShadow(
+    offset: Offset(0, 11),
+    blurRadius: 15,
+    spreadRadius: -7,
+    color: Color.fromRGBO(0, 0, 0, 0.20),
+  ),
+];
