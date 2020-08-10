2a8808fb34869e0411746d23fab83d1a124f600c
KiritchoukC <clement.kiritchouk@gmail.com>
2020-08-10 21:03:21 +0200

    feat(routing-animation): animate delete dialog
    re #56

--- lib/presentation/pages/workout/workout_page.dart
+++ lib/presentation/pages/workout/workout_page.dart
@@ -1,3 +1,4 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -46,7 +47,7 @@ class WorkoutPage extends StatelessWidget {
           actions: [
             RemoveButton(
               onPressed: () {
-                showDialog(
+                showModal(
                   context: context,
                   builder: (BuildContext context) {
                     return DeleteConfirmDialog(exerciseIds: [exercise.id]);
