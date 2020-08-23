c102d98c7867f16a547ef513029f52075d68d877
KiritchoukC <clement.kiritchouk@gmail.com>
2020-08-23 21:47:20 +0200

    feat(add-rework): animate add button
    re #105

--- lib/presentation/pages/exercise/dashboard/widgets/exercise_add/add_button.dart
+++ lib/presentation/pages/exercise/dashboard/widgets/exercise_add/add_button.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:power_progress/application/exercise/add/exercise_add_cubit.dart';
+import 'package:power_progress/presentation/pages/exercise/dashboard/widgets/exercise_add/exercise_add_animation.dart';
 import 'package:power_progress/presentation/widgets/main_button.dart';
 
 class AddButton extends StatelessWidget {
@@ -9,30 +10,11 @@ class AddButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<ExerciseAddCubit, ExerciseAddState>(
       builder: (context, state) {
-        Widget addButton() {
-          return MainButton(
-            onPressed: () {
-              context.bloc<ExerciseAddCubit>().showForm();
-            },
-            icon: Icons.add,
-          );
-        }
+        Widget addButton() => _AddButton();
 
-        Widget validateButton() {
-          return FloatingActionButton(
-            onPressed: () {
-              context.bloc<ExerciseAddCubit>().validationRequired();
-            },
-            child: const Icon(Icons.check),
-          );
-        }
+        Widget validateButton() => _ValidateButton();
 
-        Widget disabledValidateButton() {
-          return const FloatingActionButton(
-            onPressed: null,
-            child: Icon(Icons.check),
-          );
-        }
+        Widget disabledValidateButton() => const _DisabledButton();
 
         return state.when(
           inital: addButton,
@@ -43,6 +25,44 @@ class AddButton extends StatelessWidget {
           formValid: addButton,
         );
       },
+    );
+  }
+}
+
+class _AddButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MainButton(
+      onPressed: () {
+        context.bloc<ExerciseAddCubit>().showForm();
+      },
+      icon: Icons.add,
+    );
+  }
+}
+
+class _ValidateButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ExerciseAddAnimation(
+      child: FloatingActionButton(
+        onPressed: () {
+          context.bloc<ExerciseAddCubit>().validationRequired();
+        },
+        child: const Icon(Icons.check),
+      ),
+    );
+  }
+}
+
+class _DisabledButton extends StatelessWidget {
+  const _DisabledButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return const FloatingActionButton(
+      onPressed: null,
+      child: Icon(Icons.check),
     );
   }
 }
